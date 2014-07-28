25a06cf46828ccb45449cf68f673be763c8f7f2d
vlopatin <vlopatin@project.com>
2014-07-28 07:12:54 +0000

    Login form

--- index.php
+++ index.php
@@ -1,9 +1,37 @@
+<html>
+ <head>
+
+ </head>
+
+ <body>
+
+  <h1 align="center">WELCOME TO MY SIMPLE FORUM !</h1>
+  <br>
+  <br>
+  <br>
+  <br>
+
+    <div align="center" border="1">
+        <b>login:</b>
+        <input type="text" name="login" size="15"><br>
+        <b>password:</b>
+        <input type="password" name="password" size="15"><br>
+        <b>enter</b>
+        <input type="submit" name="goDo" value=" log in "><br>
+        <b>not registered?</b>
+        <input type="button" name="registerUser" value=" new user " size="50">
+    </div>
+    </body>
+
+
+
+
+</html>
+
+
+
+
+
 <?php
-
-phpinfo();
-
-
-
-#echo('Hello world !!');
-
+#phpinfo();
 ?>
