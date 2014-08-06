36b68df22efd09cbdf5394d6451757622b911dbc
vlopatin <9shi4ek@gmail.com>
2014-08-06 23:32:50 +0700

    Update model_login.php

--- models/model_login.php
+++ models/model_login.php
@@ -5,3 +5,16 @@
  * Date: 29.07.14
  * Time: 13:24
  */ 
+abstract Class Model_Login extends Model
+{
+    static function is_registered($login, $pass)
+    {
+        $mysqli = mysqli_get();
+        $sql = " SELECT * FROM logins WHERE login = '$login' AND password = '$pass' ";
+        $result = mysqli_query($mysqli, $sql);
+        
+        return $result;
+    }
+}
+
+?>
