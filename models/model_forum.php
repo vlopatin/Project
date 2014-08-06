8db4286b64a96f3d9c3dbf3bbb78ae0e27fccd05
vlopatin <9shi4ek@gmail.com>
2014-08-07 00:12:04 +0700

    Update model_forum.php

--- models/model_forum.php
+++ models/model_forum.php
@@ -5,19 +5,40 @@
  * Date: 29.07.14
  * Time: 13:24
  */
+abstract class Model_Forum
+{
 
-/*
 function add_new_post($author, $title, $message)
 {
-    //$mysqli = mysqli_get();
-    $author = mysqli_real_escape_string($mysqli, $author);
-    $title = mysqli_real_escape_string($mysqli, $title);
-    $message = mysqli_real_escape_string($mysqli, $message);
-
+    $mysqli = mysqli_get();
     $sql = "INSERT INTO posts (pid, author, title, message) VALUES (NULL, '$author', '$title', '$message' ) ";
     mysqli_query($mysqli, $sql);
 }
-*/
+
+function delete_post($pid)
+    {
+        $mysqli = mysqli_get();
+        $sql = " DELETE FROM posts WHERE pid = '$pid' ";
+        mysqli_query($mysqli, $sql);
+    }
+
+function get_posts_number()
+    {
+        $mysqli = mysqli_get();
+        $sql = " SELECT COUNT(*) FROM posts ";
+        return mysqli_query($mysqli, $sql);
+    }
+
+
+
+
+function get_posts($page=1)
+{
+    $mysqli = mysqli_get();
+    $sql = " SELECT * FROM posts ORDER BY pid desc LIMIT " . ($page - 1) * DEFAULT_NUMBER_POSTS_ON_PAGE . ", " . DEFAULT_NUMBER_POSTS_ON_PAGE;
+    return mysqli_query($mysqli, $sql);
+}
+
 
 function is_post_liked($sesUser, $pidPost)
 {
@@ -38,10 +59,9 @@ function get_likes_number($pidPost)
     $sql = " SELECT COUNT(*) FROM likes WHERE pidPost = '$pidPost' ";
     $result = mysqli_query($mysqli, $sql);
     $numberOflikes = mysqli_fetch_row($result);
-
     return $numberOflikes[0];
 
 }
-
+}
 
 ?>
