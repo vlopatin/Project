eb3265250398ad8c552427e5d1c2f0e44d38bc64
vlopatin <9shi4ek@gmail.com>
2014-08-07 23:47:05 +0700

    Update login_view.php

--- views/login_view.php
+++ views/login_view.php
@@ -6,51 +6,24 @@ if ($_SESSION['user'] !== 'Anon') {
 
 ?>
 
-
-<div align="center">
-    <a href="/register">New user</a> - <a href="/forum/Guest">Guest</a>
+<div align="center" style="background:#CCFFCC">
+	<a href="/register">New user</a> - <a href="/forum/Guest">Guest</a>
 </div>
 
-
-<h2 align="center">Войдите или зарегистрируйтесь</h2>
-
-<div align="center" border=5>
-
-    <form action="login" method="post">
-        <table>
-            <tr>
-                <td align="right">
-                    <!--      <b>login:</b> -->
-                </td>
-                <td>
-                    <input type="text" placeholder="Name" name="login" size="15" required="true">
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <!--     <b>password:</b> -->
-                </td>
-                <td>
-                    <input type="password" placeholder="Password" name="password" size="15" required="true">
-                </td>
-            </tr>
-            <tr align="center">
-                <td></td>
-                <td>
-                    <input type="submit" name="doGo" value="sign in">
-                </td>
-            </tr>
-        </table>
-    </form>
-    <hr width="30%">
-
+<div align=center style="background:#99FF99; width:200px; margin-left: auto; margin-right: auto; position:relative; top:150px;">
+    <h3 align="center" style="color:#006600">Sign-in</h3>
+   	<form action="login" method="post">
+        <input type="text" style="width:150px" placeholder="Name" name="login" required="true">
+        <input style="width:150px" type="password" placeholder="Password" name="password" required="true">
+        <input style="width:150px" type="submit" name="doGo" value="sign in">
+	</form>
     <br>
 
-    <?php
+<?php
 
-    if (isset($_POST['doGo'])) Controller_Login::action_signIn();
+if (isset($_POST['doGo'])) Controller_Login::action_signIn();
 
-    ?>
+?>
 
 </div>
 
